8d8e782d76704e02a64c79f1923d789df8d8c1b0
jagath-sk <jagathjaggu.js@gmail.com>
2025-07-17 18:30:52 +0530

    Update Service.js

--- Service.js
+++ Service.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors');
+//const cors = require('cors');
 const path = require('path');
 require('dotenv').config(); // Load from .env
 
